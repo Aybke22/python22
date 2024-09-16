4ab471d4bd7ee177715c5daf993a709d99a3b87c
Aybüke Demirci <aybukedemirci222@gmail.com>
2024-09-17 01:12:20 +0300

    Add files via upload

---
+++ string-method.py
@@ -0,0 +1,14 @@
+message='Hello There.My name is Aybüke Demirci.'
+
+#message=message.lower()
+#message=message.upper()
+#message.strip()
+#message=message.title()
+#message=message.capitalize()
+#message=message.split()
+#message=message.replace(' ' , '<3')---> Boşluk yerlerini bulup kalp ile değiştir ifadesi.
+message=' '.join(message)
+
+print(message)
+
+
